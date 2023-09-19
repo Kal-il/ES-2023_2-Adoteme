7a64cf61c574572cf134179fda2abd0732889864
Kalil Garcia Canuto <kalilgarcia38@gmail.com>
2023-09-19 19:20:36 -0300

    refactor : Adição de icones, footer, mudança nos espaçamentos e na paleta de cores da página, adiçao de mensagem e imagem na pagina home

--- src/view/pages/LoginPage.php
+++ src/view/pages/LoginPage.php
@@ -1,53 +1,67 @@
 <?php
-    /**
-        * Verficar se existe erro na url, se existir, mostrar mensagem de erro
-    */
-    if(isset($_GET['error']) && $_GET['error'] == 400){
-        echo "<h1>Não existe usuário com essas credenciais. Tente novamente</h1>";
-    }
+
+/**
+ * Verficar se existe erro na url, se existir, mostrar mensagem de erro
+ */
+if (isset($_GET['error']) && $_GET['error'] == 400) {
+    echo "<h1>Não existe usuário com essas credenciais. Tente novamente</h1>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>login-Adoteme</title>
+
 </head>
 
-<body>
-    
-    <header class="header">
-        <img class = "image" src="assets/adoteme.png" alt="Logo Adotme" width="70" height="70">
+<header class="header">
+        <a href="HomePage.php">
+        <img class="image" src="assets/adoteme.png" alt="Logo Adotme" width="100" height="100">
+        </a>
         <h1 class="adoteme">Adoteme</h1>
     </header>
-    <div class="container"> 
-       
+<body>
+    <div class="container">
+
         <div class="form-container">
-            <div class="teste"><h1>Log-In</h1></div>
+            <div class="teste">
+                <h1>Log-In</h1>
+            </div>
             <form class="form" action="" method="POST">
-                
-                
-                <p>
-                    <input class="input" type="text" name="email"  placeholder="Email">
-                </p>
-                <p>
-                    <input class="input" type="password" name="senha" placeholder="Senha">
-                </p>
-                <p>
-                    <div class="botoes">
+
+                <div class="imput-container">
+                    <div class="imput-icon">
+                        <i class="fa-solid fa-user"></i>
+                        <input class="input" type="text" name="email" placeholder="E-mail">
+                    </div>
+                    <div class="imput-icon">
+                        <i class="fa-solid fa-lock"></i>
+                        <input class="input" type="password" name="senha" placeholder="Senha">
+                    </div>
+                </div>
+ 
+                <div class="botoes">
                     <button class="entrar" type="submit">Entrar</button>
                     <button class="registro" type="submit">Novo por aqui? Registrar-se</button>
                     <button class="esqueci" type="submit">Esqueci minha senha</button>
-                    </div>
-                </p>
+                </div>
+
             </form>
         </div>
     </div>
 </body>
+<footer class="footer">
+    <p >&copy;2023 Adoteme </p>
+</footer>
 
 </html>
