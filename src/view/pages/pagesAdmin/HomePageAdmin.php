f149bc7cf1372caa45a8839efc8fab8c77b818cb
Maria Clara Nazareno Aires <claranaires@gmail.com>
2023-10-03 18:30:21 -0300

    Feature: Criação do botão que linka a página home do admin à tabela de gatos cadastrados

--- src/view/pages/pagesAdmin/HomePageAdmin.php
+++ src/view/pages/pagesAdmin/HomePageAdmin.php
@@ -1,5 +1,5 @@
 <?php
-    require '..\..\..\vendor\autoload.php';
+    require '..\..\..\..\vendor\autoload.php';
     
     use Firebase\JWT\JWT;
     use Firebase\JWT\Key;
@@ -23,6 +23,7 @@
     <h1> Futura página inicial</h1>
     <h2> Oi, admin!!!</h2>
     <button><a href="CadastrarGato.php">Adicionar Gatos</a></button>
+    <button><a href="TabelaGato.php">Tabela de Gatos</a></button>
     
 </head>
 <body>
