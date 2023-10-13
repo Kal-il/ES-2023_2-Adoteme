cfa7bb6a3ff0c066bcb3f426538c7e0393ca8366
Kalil Garcia Canuto <kalilgarcia38@gmail.com>
2023-10-13 18:56:08 -0300

    fix: :bug: correção de pequeno erro

--- src/view/pages/HomePage.php
+++ src/view/pages/HomePage.php
@@ -1,3 +1,19 @@
+<?php
+require '..\..\..\vendor\autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+if (isset($_COOKIE['jwt_token'])) {
+    $jwt_token = $_COOKIE['jwt_token'];
+    $decoded = JWT::decode($jwt_token, new Key("test_key", 'HS256'));
+
+    $decoded_array = (array) $decoded;
+    echo $decoded_array['email'];
+} else {
+    echo "<h1> faça login </h1>";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
