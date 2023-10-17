b6be3c89093197893993fc4d975388110d5d840c
Kalil Garcia Canuto <kalilgarcia38@gmail.com>
2023-10-17 19:36:47 -0300

    Modificações na parte visual do filtro e implementação de modificações no botão de curtir

--- src/view/pages/HomePage.php
+++ src/view/pages/HomePage.php
@@ -1,19 +1,3 @@
-<?php
-require '..\..\..\vendor\autoload.php';
-
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
-if (isset($_COOKIE['jwt_token'])) {
-    $jwt_token = $_COOKIE['jwt_token'];
-    $decoded = JWT::decode($jwt_token, new Key("test_key", 'HS256'));
-
-    $decoded_array = (array) $decoded;
-    echo $decoded_array['email'];
-} else {
-    echo "<h1> faça login </h1>";
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,145 +28,145 @@ if (isset($_COOKIE['jwt_token'])) {
                     filter_list
                 </span>
             </button>
-            <!-- Formulário aqui-->
-            <form id="my-form" style="display: none;">
-                <table>
-                    <thead>
-                        <!-- aqui ficam os titulos da tabéla-->
-                        <tr>
-                            <th>Cor</th>
-                            <th>Sexo</th>
-                            <th>Porte</th>
-                            <th>Idade</th>
-                            <th>Temperamento</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <!-- Cor-->
-                            <td>
-                                <label for="preto">Preto</label>
-                                <input type="checkbox" name="preto" id="preto">
-                            </td>
-                            <!-- Sexo-->
-                            <td>
-                                <label for="femea">Femea</label>
-                                <input type="checkbox" name="femea" id="femea">
-                            </td>
-
-                            <!-- Porte-->
-                            <td>
-                                <label for="pequeno">Pequeno</label>
-                                <input type="checkbox" name="pequeno" id="pequeno">
-                            </td>
-                            <!-- Idade-->
-                            <td>
-                                <label for="filhote">Filhote</label>
-                                <input type="checkbox" name="filhote" id="filhote">
-                            </td>
-                            <!-- Temperamento -->
-                            <td>
-                                <label for="calmo">Calmo</label>
-                                <input type="checkbox" name="calmo" id="calmo">
-                            </td>
-                        </tr>
-                        <tr>
-                            <!-- Cor linha 2-->
-                            <td>
-                                <label for="laranja">Laranja</label>
-                                <input type="checkbox" name="laranja" id="laranja">
-                            </td>
-                            <!-- Sexo linha 2 -->
-                            <td>
-                                <label for="macho">Macho</label>
-                                <input type="checkbox" name="macho" id="macho">
-                            </td>
-                            <!-- Porte linha 2 -->
-                            <td>
-                                <label for="medio">Médio</label>
-                                <input type="checkbox" name="medio" id="medio">
-                            </td>
-                            <td>
-
-                            </td>
-                            <!-- Temperamento linha 2 -->
-                            <td>
-                                <label for="brincalhao">Brincalhão</label>
-                                <input type="checkbox" name="brincalhao" id="brincalhao">
-                            </td>
-                        </tr>
-                        <tr>
-                            <!-- Cor linha 3-->
-                            <td>
-                                <label for="branco">Branco</label>
-                                <input type="checkbox" name="branco" id="branco">
-                            </td>
-                            <td></td>
-                            <td>
-                                <label for="grande">Grande</label>
-                                <input type="checkbox" name="grande" id="grande">
-                            </td>
-                            <td>
-                                <label for="adulto">Adulto</label>
-                                <input type="checkbox" name="adulto" id="adulto">
-
-                            </td>
-                            <td>
-                                <label for="amigavel">Amigável</label>
-                                <input type="checkbox" name="amigavel" id="amigavel">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="marrom">Marrom</label>
-                                <input type="checkbox" name="marrom" id="marrom">
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <label for="idoso">Idoso</label>
-                                <input type="checkbox" name="idoso" id="idoso">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="cinza">Cinza</label>
-                                <input type="checkbox" name="cinza" id="cinza">
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <label for="timido">Tímido</label>
-                                <input type="checkbox" name="timido" id="timido">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="rajado">Rajado</label>
-                                <input type="checkbox" name="rajado" id="rajado">
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <label for="curioso">Curioso</label>
-                                <input type="checkbox" name="curioso" id="curioso">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="tricolor">Tricolor</label>
-                                <input type="checkbox" name="tricolor" id="tricolor">
-                            </td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
         </section>
+        <!-- Formulário aqui-->
+        <form id="my-form">
+            <table>
+                <thead>
+                    <!-- aqui ficam os titulos da tabéla-->
+                    <tr>
+                        <th>Cor</th>
+                        <th>Sexo</th>
+                        <th>Porte</th>
+                        <th>Idade</th>
+                        <th>Temperamento</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <!-- Cor-->
+                        <td>
+                            <label for="preto">Preto</label>
+                            <input type="checkbox" name="preto" id="preto">
+                        </td>
+                        <!-- Sexo-->
+                        <td>
+                            <label for="femea">Femea</label>
+                            <input type="checkbox" name="femea" id="femea">
+                        </td>
+
+                        <!-- Porte-->
+                        <td>
+                            <label for="pequeno">Pequeno</label>
+                            <input type="checkbox" name="pequeno" id="pequeno">
+                        </td>
+                        <!-- Idade-->
+                        <td>
+                            <label for="filhote">Filhote</label>
+                            <input type="checkbox" name="filhote" id="filhote">
+                        </td>
+                        <!-- Temperamento -->
+                        <td>
+                            <label for="calmo">Calmo</label>
+                            <input type="checkbox" name="calmo" id="calmo">
+                        </td>
+                    </tr>
+                    <tr>
+                        <!-- Cor linha 2-->
+                        <td>
+                            <label for="laranja">Laranja</label>
+                            <input type="checkbox" name="laranja" id="laranja">
+                        </td>
+                        <!-- Sexo linha 2 -->
+                        <td>
+                            <label for="macho">Macho</label>
+                            <input type="checkbox" name="macho" id="macho">
+                        </td>
+                        <!-- Porte linha 2 -->
+                        <td>
+                            <label for="medio">Médio</label>
+                            <input type="checkbox" name="medio" id="medio">
+                        </td>
+                        <td>
+
+                        </td>
+                        <!-- Temperamento linha 2 -->
+                        <td>
+                            <label for="brincalhao">Brincalhão</label>
+                            <input type="checkbox" name="brincalhao" id="brincalhao">
+                        </td>
+                    </tr>
+                    <tr>
+                        <!-- Cor linha 3-->
+                        <td>
+                            <label for="branco">Branco</label>
+                            <input type="checkbox" name="branco" id="branco">
+                        </td>
+                        <td></td>
+                        <td>
+                            <label for="grande">Grande</label>
+                            <input type="checkbox" name="grande" id="grande">
+                        </td>
+                        <td>
+                            <label for="adulto">Adulto</label>
+                            <input type="checkbox" name="adulto" id="adulto">
+
+                        </td>
+                        <td>
+                            <label for="amigavel">Amigável</label>
+                            <input type="checkbox" name="amigavel" id="amigavel">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="marrom">Marrom</label>
+                            <input type="checkbox" name="marrom" id="marrom">
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <label for="idoso">Idoso</label>
+                            <input type="checkbox" name="idoso" id="idoso">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="cinza">Cinza</label>
+                            <input type="checkbox" name="cinza" id="cinza">
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <label for="timido">Tímido</label>
+                            <input type="checkbox" name="timido" id="timido">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="rajado">Rajado</label>
+                            <input type="checkbox" name="rajado" id="rajado">
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <label for="curioso">Curioso</label>
+                            <input type="checkbox" name="curioso" id="curioso">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="tricolor">Tricolor</label>
+                            <input type="checkbox" name="tricolor" id="tricolor">
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
 
 
         <script>
@@ -196,61 +180,47 @@ if (isset($_COOKIE['jwt_token'])) {
                     form.style.display = 'none';
                 }
             });
+            //Função de curtida
+            $(document).ready(function() {
+                $("#heart").click(function() {
+                    if ($("#heart").hasClass("liked")) {
+                        $("#heart").html('<i class="material-symbols-outlined" aria-hidden="true"></i>');
+                        $("#heart").removeClass("liked");
+                    } else {
+                        $("#heart").html('<i class="material-symbols-outlined" aria-hidden="true"></i>');
+                        $("#heart").addClass("liked");
+                    }
+                });
+            });
         </script>
 
         <section class="gallery">
             <div class="polaroide">
-                <img class="img-test" src="../assets/gato2.jpg" alt="imagem de gato">
-                <p class="info-cat">Gatinho exemplo
-                    <span class="material-symbols-outlined">
-                        favorite
-                    </span>
-                </p>
-            </div>
-            <div class="polaroide">
                 <img class="img-test" src="../assets/images.jpeg" alt="imagem de gato">
-                <p class="info-cat">Gatinho exemplo
-                    <button class="favorite-button">
-                        <span class="material-symbols-outlined">
-                            favorite
-                        </span>
-                    </button>
-                </p>
-            </div>
-            <div class="polaroide">
-                <img class="img-test" src="../assets/gato2.jpg" alt="imagem de gato">
-                <p class="info-cat">Gatinho exemplo
-                    <span class="material-symbols-outlined">
-                        favorite
-                    </span>
-                </p>
-            </div>
-            <div class="polaroide">
-                <img class="img-test" src="../assets/gato2.jpg" alt="imagem de gato">
-                <p class="info-cat">Gatinho exemplo
-                    <span class="material-symbols-outlined">
-                        favorite
-                    </span>
-                </p>
-            </div>
-            <div class="polaroide">
-                <img class="img-test" src="../assets/gato2.jpg" alt="imagem de gato">
-                <p class="info-cat">Gatinho exemplo
-                    <span class="material-symbols-outlined">
-                        favorite
-                    </span>
-                </p>
-            </div>
-            <div class="polaroide">
-                <img class="img-test" src="../assets/gato2.jpg" alt="imagem de gato">
-                <p class="info-cat">Gatinho exemplo
-                    <span class="material-symbols-outlined">
-                        favorite
-                    </span>
-                </p>
+                <div class="info-cat"> <!-- O nome do gato e o botão de curtif ficam aqui-->
+                    <p>Gatinho exemplo</p>
+                    <div class="heartbox">
+                        <input type="checkbox" class="checkbox" id="checkbox" />
+                        <label for="checkbox">
+                            <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
+                                <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
+                                    <path d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" id="heart" fill="#AAB8C2" />
+                                    <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5" />
+                                    <g id="heartgroup1" opacity="0" transform="translate(24)">
+                                        <circle id="heart1" fill="#9FC7FA" cx="2.5" cy="3" r="2" />
+                                        <circle id="heart2" fill="#9FC7FA" cx="7.5" cy="2" r="2" />
+                                    </g>
+                                </g>
+                            </svg>
+                        </label>
+                    </div>
+                </div>
             </div>
 
+
         </section>
+
+
     </main>
     <footer>
         <p>&copy;2023 Adoteme </p>
