a0dfada1c9f317ec61466244a37b58d919bba7fa
Ana Flavia Moreira Pires <flavia.pires@mail.uft.edu.br>
2023-10-17 11:06:12 -0300

    feat: :sparkles: implementando controller para search

--- src/controller/HomePageController.php
+++ src/controller/HomePageController.php
@@ -8,8 +8,24 @@ use models\Connection;
 use models\GatosModel;
 
 
-class HomePageController{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["search"])) {
+        $data = $_POST["search"];
+        $homePageController = new HomePageController();
+        $gatosInfo = $homePageController->searchGato($data);
 
+        if (!$gatosInfo) {
+            echo "<h1>Não existe gato com essa informação</h1>";
+        } else {
+            session_start();
+            $_SESSION['search_resultados'] = $gatosInfo;
+            header("Location: ../view/pages/HomePage.php");
+ 
+        }
+    }
+}
+
+class HomePageController{
 
     function infoHomeGatos(){
         $connection = new Connection();
@@ -18,4 +34,26 @@ class HomePageController{
         $gatosInfo = $gatos->ListarGatos($connection);
         return $gatosInfo;
     }
+
+    // function infoHomeGatosAdotados(){
+    //     $connection = new Connection();
+    //     $connection = $connection->getConnection();
+    //     $gatos = new GatosModel();
+    //     $gatosInfo = $gatos->ListarGatosAdotados($connection);
+    //     return $gatosInfo;
+    // }
+
+    function searchGato($data){
+        $connection = new Connection();
+        $connection = $connection->getConnection();
+        $gatos = new GatosModel();
+        $gatosInfo = $gatos->SearchGato($connection, $data);
+
+        if ($gatosInfo) 
+            return $gatosInfo;
+        return false;
+           
+    }
+
+
 }
