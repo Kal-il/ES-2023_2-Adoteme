c418a55bda250ce586470afec8062e6b98a8dc1f
Kalil Garcia Canuto <kalilgarcia38@gmail.com>
2023-10-22 01:03:23 -0300

    correção de pequenos bugs na interface visual

--- src/view/pages/HomePage.php
+++ src/view/pages/HomePage.php
@@ -1,4 +1,4 @@
-<?php
+<?/*php
 require '..\..\..\vendor\autoload.php';
 require_once __DIR__ . '..\..\..\controller\HomePageController.php';
 
@@ -30,9 +30,9 @@ if (isset($_SESSION['search_resultados'])) {
 } else {
     $homePage = new HomePageController();
     $data = $homePage->infoHomeGatos();
-}
 
-?>
+}*/ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -41,6 +41,7 @@ if (isset($_SESSION['search_resultados'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/HomePage.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>HomePage</title>
 </head>
@@ -210,41 +211,61 @@ if (isset($_SESSION['search_resultados'])) {
 
             toggleButton.addEventListener('click', () => {
                 if (form.style.display === 'none') {
-                    form.style.display = 'block';
+                    form.style.display = 'flex';
                 } else {
                     form.style.display = 'none';
                 }
             });
+            //Função de curtida
+            $(document).ready(function() {
+                $("#heart").click(function() {
+                    if ($("#heart").hasClass("liked")) {
+                        $("#heart").html('<i class="material-symbols-outlined" aria-hidden="true"></i>');
+                        $("#heart").removeClass("liked");
+                    } else {
+                        $("#heart").html('<i class="material-symbols-outlined" aria-hidden="true"></i>');
+                        $("#heart").addClass("liked");
+                    }
+                });
+            });
         </script>
 
         <section class="gallery">
-            <?php foreach ($data as $gato) : ?>
-                <div class="polaroide">
-                    <?php if (!empty($gato['foto1'])) : ?>
-                        <img class="img-test" src="<?php echo '..\..\view\pages\pagesAdmin\\' . $gato['foto1']; ?>" alt="Imagem de gato">
-                    <?php else : ?>
-                        <img class="img-test" src="../assets/gato2.jpg" alt="Imagem de gato como exemplo">
-                    <?php endif; ?>
-                    <div class="info-cat"> <!-- O nome do gato e o botão de curtif ficam aqui-->
-                        <p>Gatinho exemplo</p>
-                        <div class="heartbox">
-                            <input type="checkbox" class="checkbox" id="checkbox" />
-                            <label for="checkbox">
-                                <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
-                                    <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
-                                        <path d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" id="heart" fill="#AAB8C2" />
-                                        <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5" />
-                                        <g id="heartgroup1" opacity="0" transform="translate(24)">
-                                            <circle id="heart1" fill="#9FC7FA" cx="2.5" cy="3" r="2" />
-                                            <circle id="heart2" fill="#9FC7FA" cx="7.5" cy="2" r="2" />
-                                        </g>
-                                    </g>
-                                </svg>
-                            </label>
-                        </div>
-                    </div>
+            <? //php// foreach ($data as $gato) : 
+            ?>
+            <div class="polaroide">
+                <? //php// if (!empty($gato['foto1'])) : 
+                ?>
+                <!-- <img class="img-test" src="<? //php echo '..\..\view\pages\pagesAdmin\\' . $gato['foto1']; 
+                                                ?>" alt="Imagem de gato">-->
+                <? //php/ else : 
+                ?>
+                <img class="img-test" src="../assets/gato2.jpg" alt="Imagem de gato como exemplo">
+                <? //php// endif; 
+                ?>
+                <div class="info-cat"> <!-- O nome do gato e o botão de curtif ficam aqui-->
+                    <? //php echo $gato['nome']; 
+                    ?>
+                <p>Gatinho exemplo</p>
+                <div class="heartbox">
+                    <input type="checkbox" class="checkbox" id="checkbox" />
+                    <label for="checkbox">
+                        <svg id="heart-svg" viewBox="467 392 58 57" xmlns="http://www.w3.org/2000/svg">
+                            <g id="Group" fill="none" fill-rule="evenodd" transform="translate(467 392)">
+                                <path d="M29.144 20.773c-.063-.13-4.227-8.67-11.44-2.59C7.63 28.795 28.94 43.256 29.143 43.394c.204-.138 21.513-14.6 11.44-25.213-7.214-6.08-11.377 2.46-11.44 2.59z" id="heart" fill="#AAB8C2" />
+                                <circle id="main-circ" fill="#E2264D" opacity="0" cx="29.5" cy="29.5" r="1.5" />
+                                <g id="heartgroup1" opacity="0" transform="translate(24)">
+                                    <circle id="heart1" fill="#9FC7FA" cx="2.5" cy="3" r="2" />
+                                    <circle id="heart2" fill="#9FC7FA" cx="7.5" cy="2" r="2" />
+                                </g>
+                            </g>
+                        </svg>
+                    </label>
                 </div>
-            <?php endforeach; ?>
+                </div>
+            </div>
+            <? //php// endforeach; 
+            ?>
         </section>
 
 
