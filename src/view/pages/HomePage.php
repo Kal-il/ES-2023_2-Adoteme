3470bbe7ac0a57ebe856130652728583a2fda370
Ana Flavia Moreira Pires <flavia.pires@mail.uft.edu.br>
2023-11-11 16:17:25 -0300

    fix: :bug: corrigindo bug na home e erro ortografico na controller

--- src/view/pages/HomePage.php
+++ src/view/pages/HomePage.php
@@ -322,9 +322,10 @@ if (isset($_SESSION['search_resultados'])) {
                     </div>
                     <script>
                         $(document).ready(function() {
-                            $('#checkbox-<?php echo $gato['id']; ?>').click(function() {
+                            $('.checkbox').click(function() {
+                                var id_gato = $(this).attr('id');
+                                id_gato = id_gato.split("-")[1];
                                 var id_usuario = <?php echo $user_id; ?>;
-                                var id_gato = <?php echo $gato['id']; ?>;
                                 var checked = $(this).is(':checked');
                                 if (id_usuario == 0) {
                                     $(this).prop('checked', false);
