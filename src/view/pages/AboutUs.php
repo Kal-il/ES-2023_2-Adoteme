8c42cbb80b8f2ba22cbf4c69c944aa44b933affe
Thiago Gonzaga dos Santos <thiagogsantos2013@gmail.com>
2023-11-16 16:17:50 -0300

    feat: implementa tela about us do projeto

---
+++ src/view/pages/AboutUs.php
@@ -0,0 +1,43 @@
+<?php
+
+require('header.php')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/AboutUs.css">
+    <link href="https://rawcdn.githack.com/hung1001/font-awesome-pro/4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
+    <title>Sobre Nós</title>
+</head>
+
+<body>
+
+    <div class="cards">
+        <div class="card-about-us"><div class="card-content">
+        <i class="fad fa-bullseye-pointer" onmouseover="this.className = 'fas fa-bullseye-pointer';" onmouseout="this.className = 'fad fa-bullseye-pointer';"></i>
+
+            <h1>Missão</h1>
+
+            Nossa missão é proporcionar um lar para os gatinhos sem teto que vagam pelo campus da nossa universidade. Acreditamos que cada gato merece um lar, e estamos comprometidos em fazer isso acontecer. Nosso objetivo é criar um ambiente seguro e acolhedor para esses animais, proporcionando-lhes cuidados médicos, alimentação adequada e carinho.
+        </div></div>
+        <div class="card-about-us">
+            <div class="card-content">
+            <i class="fad fa-users" onmouseover="this.className = 'fas fa-users';" onmouseout="this.className = 'fad fa-users';"></i>
+            <h1>Equipe</h1>
+
+            Nossa equipe é composta por estudantes apaixonados pela causa animal. Cada membro da equipe contribui com suas habilidades e conhecimentos para garantir que nossos gatinhos recebam o melhor cuidado possível. Nosso trabalho é voluntário e é motivado pelo amor e compaixão que sentimos por esses animais.
+        </div></div>
+        <div class="card-about-us"><div class="card-content">
+        <i class="fad fa-cat" onmouseover="this.className = 'fas fa-cat';" onmouseout="this.className = 'fad fa-cat';"></i>
+            <h1>Gatinhos</h1>
+            Os gatinhos que adotamos são resgatados de situações de risco e trazidos para nosso campus para serem cuidados. Eles são alimentados, vacinados e cuidados com carinho até que encontrem um lar permanente. Nosso objetivo é garantir que cada gato encontre um lar amoroso e seguro.
+        </div></div>
+    </div>
+
+</body>
+
+</html>
