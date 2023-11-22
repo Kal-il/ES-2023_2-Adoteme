fe357506efde4568f8b89091a32c24e277809775
Kalil Garcia Canuto <kalilgarcia38@gmail.com>
2023-11-22 03:07:09 -0300

    feat: pequenas alterações de estilo

--- src/view/pages/PerfilPage.php
+++ src/view/pages/PerfilPage.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>x
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="/src/view/css/PerfilPage.css">
     <title>Perfil</title>
 </head>
@@ -25,40 +25,51 @@
             </a>
             <!-- Botão de voltar -->
             <div class="perfil" style="display: flex">
-                <img id="imgPerfil" src="https://cdn.dribbble.com/users/5592443/screenshots/14279501/drbl_pop_r_m_rick_4x.png" alt="imagem de perfil">
+                <?php
+                if ($usuario['foto'] == null) {
+                    echo '
+					<div class="child">
+						<img class="profile-pic"id="imgPerfil" src="https://i.pinimg.com/236x/67/58/c3/6758c3e7fa00dcd373f316ddde78bbf4.jpg" alt="Profile Picture">
+					</div>';
+                } else {
+                    echo '
+					<div class="child">
+						<img class="profile-pic"id="imgPerfil" src="/src/view/pages/pagesAdmin/' . $usuario['foto'] . '" alt="Profile Picture">
+					</div>';
+                }
+                ?>
                 <div id="perfil_info">
-                    <p id="nome_user">Rick Snches</p>
+                    <p id="nome_user"><?php echo $usuario['nome'] ?></p>
                     <div id="perfil_info2">
-                        <p id="email_user">Sanches@gmail.com</p>
-                        <P id="cidade"> Palmas-TO</p>
+                        <p id="email_user"><?php echo $usuario['email'] ?></p>
+                        <P id="cidade"><?php echo $usuario['cidade'] ?></p>
                     </div>
                 </div>
-            </div>
 
-            <div class="botoes">
-                <a href="/src/view/pages/AcompanhamentoPage.php">
-                    <button id="botão1">
-                        <img src="/src/view/assets/Frame 124.png" alt="" width="70" height="60">
-                        Adoções em progresso
-                    </button>
-                </a>
-                <a href="/src/view/pages/FavoritosPage.php">
-                    <button id="botão2">
-                        <img src="/src/view/assets/heart.png" alt="" width="80" height="60">
-                        Gatos Favoritos
-                    </button>
-                </a>
-                <a href="/src/view/pages/EditarPerfil.php">
-                    <button id="botão3">
-                        <img src="/src/view/assets/Account.png" alt="" width="90" height="60">
-                        Editar perfil
-                    </button>
-                </a>
-            </div>
-            <div class="seusInteresses">
-                <p>Seus Interesses</p>
-                <!---->
-            </div>
+                <div class="botoes">
+                    <a href="/src/view/pages/AcompanhamentoPage.php">
+                        <button id="botão1">
+                            <img src="/src/view/assets/Frame 124.png" alt="" width="70" height="60">
+                            Adoções em progresso
+                        </button>
+                    </a>
+                    <a href="/src/view/pages/FavoritosPage.php">
+                        <button id="botão2">
+                            <img src="/src/view/assets/heart.png" alt="" width="80" height="60">
+                            Gatos Favoritos
+                        </button>
+                    </a>
+                    <a href="/src/view/pages/EditarPerfil.php">
+                        <button id="botão3">
+                            <img src="/src/view/assets/Account.png" alt="" width="90" height="60">
+                            Editar perfil
+                        </button>
+                    </a>
+                </div>
+                <div class="seusInteresses">
+                    <p>Seus Interesses</p>
+                    <!---->
+                </div>
 
         </main>
         <footer class="footer">
--- src/view/partials/Header.php
+++ src/view/partials/Header.php
@@ -7,6 +7,7 @@
 	}
 
 	#logo {
+		width: 95vw;
 		padding-left: 5px;
 		padding-right: 5px;
 		padding-top: 5px;
